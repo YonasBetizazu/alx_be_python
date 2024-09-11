ebf0040ae69d497dcc5be289c4797fc4ef4dbfe6
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-09-11 14:55:50 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-p=1000    #( P ) is the principal amount (initial investment),
-r=0.05    #representing 5% annual interest rate),
-t=3        #representing 3 years
-i = p*r*t    #Calculate the simple interest using the formula provided 
-print("The simple interest is " +str(i))
+principal =1000    #( P ) is the principal amount (initial investment),
+rate =0.05    #representing 5% annual interest rate),
+time =3        #representing 3 years
+interest = principal *rate *time     #Calculate the simple interest using the formula provided 
+print("The simple interest is: " +str(interest))
