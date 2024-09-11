48b69baf3921d995adfeb9bf236243eeb0597b54
[Yonas <Yonbetizazu@gmail.com>
2024-09-11 14:47:38 +0200

    build simple app for calcluating simple interst

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+p=1000    #( P ) is the principal amount (initial investment),
+r=0.05    #representing 5% annual interest rate),
+t=3        #representing 3 years
+i = p*r*t    #Calculate the simple interest using the formula provided 
+print("The simple interest is " +str(i))
