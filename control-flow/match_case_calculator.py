51d18b025a53e7d58491cf092f4018514cb60c77
[Yonas <Yonbetizazu@gmail.com>
2024-09-17 14:53:33 +0200

    finished the task 0-3

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+first_number = int(input("Enter the first number: "))
+secound_number = int(input("Enter the second number: "))
+type_of_operation = input("Choose the operation (+, -, *, /) ")
+if type_of_operation=="+":
+    print(f"The result is {first_number+secound_number}.")
+elif type_of_operation=="-":
+    print(f"The result is {first_number-secound_number}.")
+elif type_of_operation=="*":
+    print(f"The result is {first_number*secound_number}.")
+elif type_of_operation=="/":
+    if secound_number==int(0):
+        print("Cannot divide by zero.")
+    else:
+        print(f"The result is {first_number/secound_number}.")
+        
+  
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,6 @@
+size_of_pattern = int(input("Enter the size of the pattern:"))
+while size_of_pattern>0:
+    print("*")
+    size_of_pattern-=1
+    
+
