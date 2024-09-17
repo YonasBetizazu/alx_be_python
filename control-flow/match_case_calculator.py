b1512474b4ce99e91a355a6bc63d1bf21872f036
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-09-17 15:00:53 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 first_number = int(input("Enter the first number: "))
 secound_number = int(input("Enter the second number: "))
-type_of_operation = input("Choose the operation (+, -, *, /) ")
+type_of_operation = input("Choose the operation (+, -, *, /):")
 if type_of_operation=="+":
     print(f"The result is {first_number+secound_number}.")
 elif type_of_operation=="-":
