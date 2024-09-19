3d59c203bf73a3e9ffeb676e94b8482bd4a7df40
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-09-19 13:21:18 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,14 +1,17 @@
 first_number = int(input("Enter the first number: "))
 secound_number = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /):")
-if operation== "+":
-    print(f"The result is {first_number+secound_number}.")
-elif operation== "-":
-    print(f"The result is {first_number-secound_number}.")
-elif operation== "*":
-    print(f"The result is {first_number*secound_number}.")
-elif operation== "/":
-    if secound_number==int(0):
-        print("Cannot divide by zero.")
-    else:
-        print(f"The result is {first_number/secound_number}.")
+
+
+match operation:
+    case "+":
+        print(f"The result is {first_number+secound_number}.")
+    case "-":
+        print(f"The result is {first_number-secound_number}.")
+    case "*":
+        print(f"The result is {first_number*secound_number}.")
+    case("/"):
+         if secound_number==0:
+             print("Cannot divide by zero.")
+         else: 
+             print(f"The result is {first_number/secound_number}.")
