5df186b69ca39afd67bf458d5c6862189d0c74ce
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-09-27 13:36:10 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,11 +3,11 @@ def perform_operation(num1,num2,operation):
         case 'add':
          return num1+num2
         case 'subtract':
-          return num2+num1
+          return num1-num2
         case 'multiply':
           return num1*num2
         case 'divide':
           if num2==0:
-             print("undfined")
+             return "undfined"
           else:
              return num1/num2
