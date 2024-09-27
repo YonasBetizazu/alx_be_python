7ac10b3db0435e6688059d95a2176741e1a73c61
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-09-27 13:21:59 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1,num2,"operation"):
     match operation:
         case 'add':
-         return float(num1)+float(num2)
+         return (num1)+(num2)
         case 'subtract':
-          return float(num2)+float(num1)
+          return (num2)+(num1)
         case 'multiply':
-          return float(num1)*float(num2)
+          return (num1)*(num2)
         case 'divide':
-          if float(num2)==0:
+          if (num2)==0:
              print("undfined")
           else:
-             return float(num1)/float(num2)
+             return (num1)/(num2)
