799b727631df290c65fb07909599a8875260bec7
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-10-06 16:30:47 +0200

    Update bank_account.py

--- programming_paradigm /bank_account.py
+++ programming_paradigm /bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.__account_balance = account_balance  
+        self.account_balance = account_balance  
 
     def deposit(self, amount):
         """Add the specified amount to the account balance."""
@@ -13,15 +13,7 @@ class BankAccount:
     def withdraw(self, amount):
         """Deduct the specified amount from the account balance if sufficient funds are available."""
         if 0 < amount <= self.__account_balance:  
-            self.__account_balance -= amount  #
+            self.__account_balance -= amount
             return True  # Withdrawal successful
-        elif amount > self.__account_balance:
-            print("You don't have sufficient funds.")
-            return False  # Withdrawal failed
-        else:
-            print("Something went wrong!")  
-            return False  # Withdrawal failed
-
     def display_balance(self):
-        """Print the current account balance."""
         print(f"Your balance is ${self.__account_balance:.2f}")  
